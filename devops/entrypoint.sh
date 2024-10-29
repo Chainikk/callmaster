1aa853b824816266dab3eabd1877ae5da728b985
skhodzhaev <skhodzhaev@gmail.com>
2024-10-29 16:36:12 +0300

    DevOps tools

---
+++ devops/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+# Синхронизация Моделей с Базой Данных
+python -c "from app.models.utils import create_db, init_db; create_db(); init_db()"
+
+# Запуск приложения
+exec python /app/app/main.py
